6d1d49376811d89e848113de5a27fdcd40909fd8
Stefan Mitterrutzner <mitterrutzner@ubique.ch>
2021-02-23 16:49:48 +0100

    adds wmts layer objects

---
+++ shared/src/map/layers/tiled/wmts/WmtsTiled2dMapLayerConfig.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2021 Ubique Innovation AG <https://www.ubique.ch>
+ *
+ *  This Source Code Form is subject to the terms of the Mozilla Public
+ *  License, v. 2.0. If a copy of the MPL was not distributed with this
+ *  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ *  SPDX-License-Identifier: MPL-2.0
+ */
+
+#pragma once
+
+#include "Tiled2dMapLayerConfig.h"
+#include "WmtsLayerConfiguration.h"
+#include "Tiled2dMapZoomInfo.h"
+#include "Tiled2dMapZoomLevelInfo.h"
+
+class WmtsTiled2dMapLayerConfig: public Tiled2dMapLayerConfig {
+public:
+    WmtsTiled2dMapLayerConfig(const WmtsLayerConfiguration &configuration,
+                              std::vector<Tiled2dMapZoomLevelInfo> zoomLevelInfo,
+                              const Tiled2dMapZoomInfo &zoomInfo);
+
+    virtual ::RectCoord getBounds() override;
+
+    virtual std::string getTileUrl(int32_t x, int32_t y, int32_t zoom) override;
+
+    virtual std::string getTileIdentifier(int32_t x, int32_t y, int32_t zoom) override;
+
+    virtual std::vector<Tiled2dMapZoomLevelInfo> getZoomLevelInfos() override;
+
+    virtual Tiled2dMapZoomInfo getZoomInfo() override;
+private:
+    const WmtsLayerConfiguration configuration;
+    std::vector<Tiled2dMapZoomLevelInfo> zoomLevelInfo;
+    const Tiled2dMapZoomInfo zoomInfo;
+};
