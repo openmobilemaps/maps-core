fdb60769db0301610d035da697b66002eb21ee6e
Christoph Maurhofer <maurhofer@ubique.ch>
2023-08-22 16:11:15 +0200

    Adjust offset calculation

--- shared/src/map/layers/tiled/vector/symbol/Tiled2dMapVectorSymbolLabelObject.cpp
+++ shared/src/map/layers/tiled/vector/symbol/Tiled2dMapVectorSymbolLabelObject.cpp
@@ -443,8 +443,8 @@ void Tiled2dMapVectorSymbolLabelObject::updatePropertiesPoint(std::vector<float>
         case Anchor::CENTER:
         case Anchor::LEFT:
         case Anchor::RIGHT:
-            anchorOffset.y = -size.y / 2.0;
-            anchorOffset.y -= -textOffset.y * fontSize + yOffset;
+            anchorOffset.y = -size.y;
+            anchorOffset.y -= textOffset.y * fontSize - yOffset;
             break;
         case Anchor::TOP:
         case Anchor::TOP_LEFT:
