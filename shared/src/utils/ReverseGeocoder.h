9fa8c9f306193263a12cb9915baa31ffe2a26b6d
Stefan Mitterrutzner <mitterrutzner@ubique.ch>
2024-06-18 17:03:40 +0200

    adds reversegeocoder

---
+++ shared/src/utils/ReverseGeocoder.h
@@ -0,0 +1,24 @@
+//
+//  ReverseGeocoder.hpp
+//  
+//
+//  Created by Stefan Mitterrutzner on 18.06.2024.
+//
+
+#pragma once
+#include "ReverseGeocoderInterface.h"
+
+class ReverseGeocoder: public ReverseGeocoderInterface {
+public:
+    ReverseGeocoder(const /*not-null*/ std::shared_ptr<::LoaderInterface> & loader, const std::string & tileUrlTemplate, int32_t zoomLevel);
+
+    virtual std::vector<::VectorLayerFeatureCoordInfo> reverseGeocode(const ::Coord & coord, int64_t thresholdMeters) override;
+
+private:
+    double distance(const Coord &c1, const Coord &c2);
+    
+    const /*not-null*/ std::shared_ptr<::LoaderInterface> loader;
+    const std::string tileUrlTemplate;
+    int32_t zoomLevel;
+};
+
