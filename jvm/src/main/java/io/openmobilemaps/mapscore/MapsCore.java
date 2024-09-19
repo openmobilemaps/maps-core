6b0273b9922cc524f767ea33f0d868d6e7360802
Matthias Frei <matzf@scion.org>
2024-09-19 13:51:44 +0200

    build: include platform string in native library filename
    Name suffix is linux_amd64 (plus _debug for debug build).

--- jvm/src/main/java/io/openmobilemaps/mapscore/MapsCore.java
+++ jvm/src/main/java/io/openmobilemaps/mapscore/MapsCore.java
@@ -1,5 +1,6 @@
 package io.openmobilemaps.mapscore;
 
+import java.util.logging.Logger;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,10 +9,26 @@ import java.nio.file.StandardCopyOption;
 
 public class MapsCore {
   public static void initialize() {
+    final var logger = Logger.getLogger(MapsCore.class.getName());
+
+    // This wont work for all platforms; it only has to work for the platforms that we build the library.
+    // Currently, that is:
+    //  - linux amd64
+    final String os = System.getProperty("os.name").toLowerCase();
+    final String arch = System.getProperty("os.arch");
+
+    final boolean debug = Boolean.getBoolean("io.openmobilemaps.mapscore.debug");
+
+    final String buildType = debug ? "_debug" : "";
+    final String libName = String.format("libmapscore_jni_%s_%s%s.so", os, arch, buildType);
+    final String libResourcePath = "/native/" + libName;
+
+    logger.config("Loading native library from resource path " + libResourcePath);
+
     try {
       File tempLib = File.createTempFile("libmapscore_jni_", ".so");
       tempLib.deleteOnExit();
-      InputStream lib = MapsCore.class.getResourceAsStream("/native/libmapscore_jni.so");
+      InputStream lib = MapsCore.class.getResourceAsStream(libResourcePath);
       Files.copy(lib, tempLib.toPath(), StandardCopyOption.REPLACE_EXISTING);
 
       System.load(tempLib.getAbsolutePath());
