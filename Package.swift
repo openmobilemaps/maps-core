309baaa95e9560a997814fae5ebe6819fc84f7c8
Stefan Mitterrutzner <mitterrutzner@ubique.ch>
2024-09-23 11:18:17 +0200

    remove _LIBCPP_DISABLE_AVAILABILITY for Xcode 16

--- Package.swift
+++ Package.swift
@@ -185,8 +185,6 @@ let package = Package(
                 .headerSearchPath("src/utils"),
                 .define("DEBUG", to: "1", .when(configuration: .debug)),
                 .define("NDEBUG", to: "1", .when(configuration: .release)),
-                .define("_LIBCPP_DISABLE_AVAILABILITY", to: "1", .when(configuration: .debug)),
-                .define("_LIBCPP_DISABLE_AVAILABILITY", to: "1", .when(configuration: .release)),
             ]
         ),
     ],
