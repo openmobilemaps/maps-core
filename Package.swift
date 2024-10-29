96f2707ac4230a67a54188ad57132fefcd2c5f92
Christoph Maurhofer <maurhofer@ubique.ch>
2024-10-29 13:31:33 +0100

    Prepare for release 3.0.0-rc.1

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/UbiqueInnovation/djinni.git", .branch("release/1.0.8")),
+        .package(url: "https://github.com/UbiqueInnovation/djinni.git", .branch("release/1.0.9")),
     ],
     targets: [
         .target(
