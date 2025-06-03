openmobilemaps/maps-core

commit 2ff6320b8d94374b401ef0a46d9dcd1ea5534827

Remove shared_ptr from VectorSet, move-almost-only
Remove the shared_ptr from VectorSet to remove one unnecessary
indirection & allocation.
To ensure that with this change we don't accidentally copy the vector
data more frequently, VectorSet is made mostly move-only type.
There is an explicit copy() method that allows to create copies where
necessary, without the risk of accidentally calling the copy
constructor/assignment.

Aside; simple optimization to the VectorSet::insertSet "algorithm".

shared/public/VectorSet.h
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <memory>
 #include <vector>
 
 #pragma once
@@ -10,40 +9,48 @@ public:
     using const_iterator = typename std::vector<T>::const_iterator;
     using iterator = typename std::vector<T>::iterator;
 
-    // Default constructor
-    VectorSet(): data(std::make_shared<std::vector<T>>()) {}
-
-    VectorSet(const VectorSet<T>& source)
-         : data(std::move(source.data))
-    {  }
+    VectorSet() = default;
+    // Move construction / assignment
+    VectorSet(VectorSet<T> &&x) = default;
+    VectorSet<T>& operator=(VectorSet<T> &&x) = default;
+    // No copy constructor/assignment (BUT see copy())
+    VectorSet(const VectorSet<T> &x) = delete;
+    VectorSet<T>& operator=(const VectorSet<T> &x) = delete;
 
     // Constructor with initializer list
-    VectorSet(std::initializer_list<T> initList)
-    : data(std::make_shared<std::vector<T>>()) {
+    VectorSet(std::initializer_list<T> initList) {
         for (const T& value : initList) {
             insert(value);
         }
     }
 
+    // Explicit copy.
+    // This type _is_ copyable, but only with an explicit copy() call to avoid unnecessary, accidental copies.
+    VectorSet<T> copy() const {
+        VectorSet<T> other;
+        other.data = std::vector<T>(data);
+        return other;
+    }
+
     // Insert an element into the set
     void insert(const T& value) {
         if (!contains(value)) {
-            data->push_back(value);
+            data.push_back(value);
         }
     }
 
     // Find an element in the set
     bool contains(const T& value) const {
-        return find(value) != data->end();
+        return find(value) != data.end();
     }
 
     const_iterator find(const T& value) const {
-        return std::find(data->begin(), data->end(), value);
+        return std::find(data.begin(), data.end(), value);
     }
 
     // Check if the set is empty
     bool empty() const {
-        return data->empty();
+        return data.empty();
     }
 
     // Check if all elements of another set are present in this set
@@ -58,28 +65,42 @@ public:
 
     // Insert elements from another VectorSet into this set
     void insertSet(const VectorSet<T>& otherSet) {
+        size_t oldSize = data.size();
         for (auto it = otherSet.begin(); it != otherSet.end(); it++) {
-            insert(*it);
+            // only check if initial set contains a value; 
+            // no need to check inserted values also if otherSet is assumed to be duplicate free
+            auto searchEnd = data.end() + oldSize;
+            if(std::find(data.begin(), searchEnd, *it) != searchEnd) {
+                data.push_back(*it);
+            }
         }
     }
 
     // Iterator functions
-    iterator begin() const {
-        return data->begin();
+    const_iterator begin() const {
+        return data.begin();
     }
 
-    iterator end() const {
-        return data->end();
+    const_iterator end() const {
+        return data.end();
+    }
+
+    iterator begin() {
+        return data.begin();
+    }
+
+    iterator end() {
+        return data.end();
     }
 
     size_t size() const {
-        return data->size();
+        return data.size();
     }
 
     void reserve(const size_t size) {
-        data->reserve(size);
+        data.reserve(size);
     }
 
 private:
-    std::shared_ptr<std::vector<T>> data;
+    std::vector<T> data;
 };

shared/public/Value.h
@@ -282,14 +282,14 @@ public:
 
     UsedKeysCollection() {};
 
-    UsedKeysCollection(const VectorSet<std::string> &usedKeys) : usedKeys(usedKeys) {};
+    UsedKeysCollection(VectorSet<std::string> &&usedKeys) : usedKeys(std::move(usedKeys)) {};
 
-    UsedKeysCollection(const VectorSet<std::string> &usedKeys,
-                       const VectorSet<std::string> &featureStateKeys,
-                       const VectorSet<std::string> &globalStateKeys)
-            : usedKeys(usedKeys),
-              featureStateKeys(featureStateKeys),
-              globalStateKeys(globalStateKeys) {};
+    UsedKeysCollection(VectorSet<std::string> &&usedKeys,
+                       VectorSet<std::string> &&featureStateKeys,
+                       VectorSet<std::string> &&globalStateKeys)
+            : usedKeys(std::move(usedKeys)),
+              featureStateKeys(std::move(featureStateKeys)),
+              globalStateKeys(std::move(globalStateKeys)) {};
 
     void includeOther(const UsedKeysCollection &other) {
         usedKeys.insertSet(other.usedKeys);
@@ -672,7 +672,7 @@ public:
     void updateValue(std::shared_ptr<Value> newValue) {
         value = newValue;
 
-        usedKeysCollection = newValue ? newValue->getUsedKeys() : UsedKeysCollection();
+        usedKeysCollection = newValue ? std::move(newValue->getUsedKeys()) : std::move(UsedKeysCollection());
 
         isStatic = usedKeysCollection.empty();
         isZoomDependent = usedKeysCollection.usedKeys.contains("zoom");
@@ -903,7 +903,7 @@ public:
                 end = res.find("}", begin);
             }
 
-            return UsedKeysCollection(usedKeys);
+            return std::move(usedKeys);
 
         } else if (std::holds_alternative<std::vector<std::string>>(value)) {
             const auto& res = std::get<std::vector<std::string>>(value);

shared/src/map/layers/tiled/vector/Tiled2dMapVectorLayer.cpp
@@ -843,7 +843,7 @@ void Tiled2dMapVectorLayer::onTilesUpdated(const std::string &layerName, VectorS
 
     auto sourceManager = sourceDataManagers.find(layerName);
     if (sourceManager != sourceDataManagers.end()) {
-        sourceManager->second.message(MailboxDuplicationStrategy::replaceNewest, MFN(&Tiled2dMapVectorSourceTileDataManager::onRasterTilesUpdated), layerName, currentTileInfos);
+        sourceManager->second.message(MailboxDuplicationStrategy::replaceNewest, MFN(&Tiled2dMapVectorSourceTileDataManager::onRasterTilesUpdated), layerName, std::move(currentTileInfos));
     }
     tilesStillValid.clear();
 }
@@ -852,13 +852,15 @@ void Tiled2dMapVectorLayer::onTilesUpdated(const std::string &sourceName, Vector
 
     std::unique_lock<std::mutex> lock(setupMutex);
     setupCV.wait(lock, [this]{ return setupReady; });
+    
     auto sourceManager = sourceDataManagers.find(sourceName);
     if (sourceManager != sourceDataManagers.end()) {
-        sourceManager->second.message(MailboxDuplicationStrategy::replaceNewest, MFN(&Tiled2dMapVectorSourceTileDataManager::onVectorTilesUpdated), sourceName, currentTileInfos);
+        auto currentTileInfosCopy = currentTileInfos.copy(); // can only be moved into one of the two messages, so make a copy first.
+        sourceManager->second.message(MailboxDuplicationStrategy::replaceNewest, MFN(&Tiled2dMapVectorSourceTileDataManager::onVectorTilesUpdated), sourceName, std::move(currentTileInfosCopy));
     }
     auto symbolSourceManager = symbolSourceDataManagers.find(sourceName);
     if (symbolSourceManager != symbolSourceDataManagers.end()) {
-        symbolSourceManager->second.message(MailboxDuplicationStrategy::replaceNewest, MFN(&Tiled2dMapVectorSourceTileDataManager::onVectorTilesUpdated), sourceName, currentTileInfos);
+        symbolSourceManager->second.message(MailboxDuplicationStrategy::replaceNewest, MFN(&Tiled2dMapVectorSourceTileDataManager::onVectorTilesUpdated), sourceName, std::move(currentTileInfos));
     }
     tilesStillValid.clear();
 }

shared/src/map/layers/tiled/vector/symbol/Tiled2dMapVectorSourceSymbolDataManager.cpp
@@ -380,7 +380,7 @@ void Tiled2dMapVectorSourceSymbolDataManager::onVectorTilesUpdated(const std::st
 
         for (const auto &[tileInfo, groupMap]: tileSymbolGroupMap) {
             bool found = false;
-            for (const auto &currentTile: currentTileInfos) {
+            for (const auto &currentTile: latestTileInfos) {
                 if (tileInfo == currentTile.tileInfo) {
                     found = true;
                     break;

shared/src/map/layers/tiled/vector/tiles/line/Tiled2dMapVectorLineTile.cpp
@@ -32,11 +32,11 @@ Tiled2dMapVectorLineTile::Tiled2dMapVectorLineTile(const std::weak_ptr<MapInterf
 void Tiled2dMapVectorLineTile::updateVectorLayerDescription(const std::shared_ptr<VectorLayerDescription> &description,
                                                       const Tiled2dMapVectorTileDataVector &tileData) {
     Tiled2dMapVectorTile::updateVectorLayerDescription(description, tileData);
-    const auto newUsedKeys = description->getUsedKeys();
+    auto newUsedKeys = description->getUsedKeys();
     bool usedKeysContainsNewUsedKeys = usedKeys.covers(newUsedKeys);
     isStyleZoomDependant = newUsedKeys.containsUsedKey(Tiled2dMapVectorStyleParser::zoomExpression);
     isStyleStateDependant = newUsedKeys.isStateDependant();
-    usedKeys = newUsedKeys;
+    usedKeys = std::move(newUsedKeys);
     lastZoom = std::nullopt;
     lastAlpha = std::nullopt;
 
@@ -46,7 +46,6 @@ void Tiled2dMapVectorLineTile::updateVectorLayerDescription(const std::shared_pt
         
         tileCallbackInterface.message(MFN(&Tiled2dMapVectorLayerTileCallbackInterface::tileIsReady), tileInfo, description->identifier, WeakActor<Tiled2dMapVectorTile>(mailbox, shared_from_this()));
     } else {
-        usedKeys = std::move(newUsedKeys);
         reusableLineStyles.clear();
         featureGroups.clear();
         styleHashToGroupMap.clear();

shared/src/map/layers/tiled/vector/tiles/polygon/Tiled2dMapVectorPolygonPatternTile.cpp
@@ -37,11 +37,11 @@ Tiled2dMapVectorPolygonPatternTile::Tiled2dMapVectorPolygonPatternTile(const std
 void Tiled2dMapVectorPolygonPatternTile::updateVectorLayerDescription(const std::shared_ptr<VectorLayerDescription> &description,
                                                          const Tiled2dMapVectorTileDataVector &tileData) {
     Tiled2dMapVectorTile::updateVectorLayerDescription(description, tileData);
-    const auto newUsedKeys = description->getUsedKeys();
+    auto newUsedKeys = description->getUsedKeys();
     bool usedKeysContainsNewUsedKeys = usedKeys.covers(newUsedKeys);
     isStyleZoomDependant = newUsedKeys.containsUsedKey(Tiled2dMapVectorStyleParser::zoomExpression);
     isStyleStateDependant = newUsedKeys.isStateDependant();
-    usedKeys = newUsedKeys;
+    usedKeys = std::move(newUsedKeys);
     lastZoom = std::nullopt;
     lastAlpha = std::nullopt;
 
@@ -51,7 +51,6 @@ void Tiled2dMapVectorPolygonPatternTile::updateVectorLayerDescription(const std:
         
         tileCallbackInterface.message(MFN(&Tiled2dMapVectorLayerTileCallbackInterface::tileIsReady), tileInfo, description->identifier, WeakActor<Tiled2dMapVectorTile>(mailbox, shared_from_this()));
     } else {
-        usedKeys = std::move(newUsedKeys);
         featureGroups.clear();
         styleHashToGroupMap.clear();
         hitDetectionPolygons.clear();

shared/src/map/layers/tiled/vector/tiles/polygon/Tiled2dMapVectorPolygonTile.cpp
@@ -34,11 +34,11 @@ Tiled2dMapVectorPolygonTile::Tiled2dMapVectorPolygonTile(const std::weak_ptr<Map
 void Tiled2dMapVectorPolygonTile::updateVectorLayerDescription(const std::shared_ptr<VectorLayerDescription> &description,
                                                          const Tiled2dMapVectorTileDataVector &tileData) {
     Tiled2dMapVectorTile::updateVectorLayerDescription(description, tileData);
-    const auto newUsedKeys = description->getUsedKeys();
+    auto newUsedKeys = description->getUsedKeys();
     bool usedKeysContainsNewUsedKeys = usedKeys.covers(newUsedKeys);
     isStyleZoomDependant = newUsedKeys.containsUsedKey(Tiled2dMapVectorStyleParser::zoomExpression);
     isStyleStateDependant = newUsedKeys.isStateDependant();
-    usedKeys = newUsedKeys;
+    usedKeys = std::move(newUsedKeys);
     lastZoom = std::nullopt;
     lastAlpha = std::nullopt;
 
@@ -48,7 +48,6 @@ void Tiled2dMapVectorPolygonTile::updateVectorLayerDescription(const std::shared
 
         tileCallbackInterface.message(MFN(&Tiled2dMapVectorLayerTileCallbackInterface::tileIsReady), tileInfo, description->identifier, WeakActor<Tiled2dMapVectorTile>(mailbox, shared_from_this()));
     } else {
-        usedKeys = std::move(newUsedKeys);
         featureGroups.clear();
         styleHashToGroupMap.clear();
         hitDetectionPolygons.clear();

commit 6aed4d50045fe41b32e01cddff394ccbd014a25c

Fixup "Remove shared_ptr from VectorSet, move-almost-only" insertSet
This fixes a bug introduced in 2ff6320b8d94374b401ef0a46d9dcd1ea5534827.

Fix bad iterator (begin() + size, instead of end() + size) and flipped
condition logic error in "optimized" insertSet.

Add a test, like I should have in the first place.

shared/public/VectorSet.h
@@ -69,8 +69,8 @@ public:
         for (auto it = otherSet.begin(); it != otherSet.end(); it++) {
             // only check if initial set contains a value; 
             // no need to check inserted values also if otherSet is assumed to be duplicate free
-            auto searchEnd = data.end() + oldSize;
-            if(std::find(data.begin(), searchEnd, *it) != searchEnd) {
+            auto searchEnd = data.begin() + oldSize;
+            if(std::find(data.begin(), searchEnd, *it) == searchEnd) {
                 data.push_back(*it);
             }
         }

shared/test/CMakeLists.txt
@@ -14,6 +14,7 @@ add_executable(tests
   "TestTileSource.cpp"
   "TestGeometryHandler.cpp"
   "TestValueEvaluate.cpp"
+  "TestVectorSet.cpp"
   "helper/TestData.cpp"
 )
 # Use mapscore _private_ include to allow testing functionality declared in internal headers.

shared/test/TestVectorSet.cpp
@@ -1,0 +1,25 @@
+#include "VectorSet.h"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_range_equals.hpp>
+
+#include <set>
+
+TEST_CASE("VectorSet<std::string>::insertSet") {
+    VectorSet<std::string> vsA{ "foo", "bar" };
+    VectorSet<std::string> vsB{ "bar", "fnord", "food" };
+    VectorSet<std::string> vsC{ "fnord" };
+    VectorSet<std::string> vsD{ };
+    VectorSet<std::string> vsE{ "napf" };
+    vsA.insertSet(vsB);
+    vsA.insertSet(vsC);
+    vsA.insertSet(vsD);
+    vsA.insertSet(vsE);
+
+    std::set<std::string> setA(vsA.begin(), vsA.end());
+    setA.insert(vsB.begin(), vsB.end());
+    setA.insert(vsC.begin(), vsC.end());
+    setA.insert(vsD.begin(), vsD.end());
+    setA.insert(vsE.begin(), vsE.end());
+    REQUIRE_THAT(vsA, Catch::Matchers::UnorderedRangeEquals(setA));
+}