02a5e72242ee7e9a12afcb042c3a0e42ac2d8d58
Marco Zimmermann <zimmermann@ubique.ch>
2025-07-07 07:34:39 +0200

    optimized lut sin cos implementation

--- shared/public/TrigonometryLUT.h
+++ shared/public/TrigonometryLUT.h
@@ -13,22 +13,36 @@
 #include <cmath>
 
 namespace lut {
-    constexpr int tableSize = 8192;
-    constexpr double twoPi = 2.0 * M_PI;
-    constexpr double invTwoPi = 1.0 / twoPi;
+    namespace {
+        constexpr int coreTableSize = 1024;
 
-    namespace detail {
-        inline std::array<double, tableSize> generateTable(bool isSin) {
-            std::array<double, tableSize> table{};
-            for (int i = 0; i < tableSize; ++i) {
-                double angle = (twoPi * i) / tableSize;
-                table[i] = isSin ? std::sin(angle) : std::cos(angle);
+        constexpr double pi        = M_PI;
+        constexpr double twoPi     = 2.0 * pi;
+        constexpr double invPi     = 1.0 / pi;
+        constexpr double invTwoPi  = 1.0 / twoPi;
+        constexpr double scale     = coreTableSize * invPi;
+        constexpr int paddedSize   = coreTableSize + 6;
+
+        // Precomputed sin/cos tables with 3-point padding
+        std::array<double, paddedSize> generateTable(bool isSin) {
+            std::array<double, paddedSize> table{};
+            for (int i = 0; i < coreTableSize; ++i) {
+                double angle = (pi * i) / coreTableSize;
+                table[i + 3] = isSin ? std::sin(angle) : std::cos(angle);
+            }
+            for (int i = -3; i < 0; ++i) {
+                double angle = (pi * i) / coreTableSize;
+                table[3 + i] = isSin ? std::sin(angle) : std::cos(angle);
+            }
+            for (int i = 0; i < 3; ++i) {
+                double angle = pi + (pi * i) / coreTableSize;
+                table[coreTableSize + 3 + i] = isSin ? std::sin(angle) : std::cos(angle);
             }
             return table;
         }
 
-        inline const std::array<double, tableSize> sinTable = generateTable(true);
-        inline const std::array<double, tableSize> cosTable = generateTable(false);
+        const auto sinTable = generateTable(true);
+        const auto cosTable = generateTable(false);
 
         inline double catmullRom(double y0, double y1, double y2, double y3, double t) {
             double a0 = -0.5 * y0 + 1.5 * y1 - 1.5 * y2 + 0.5 * y3;
@@ -38,49 +52,45 @@ namespace lut {
             return ((a0 * t + a1) * t + a2) * t + a3;
         }
 
-        inline double interpolate(const std::array<double, tableSize>& table, double x) {
-            x -= std::floor(x * invTwoPi) * twoPi;  // wrap into [0, 2π)
-            double indexF = x * (tableSize * invTwoPi);
+        struct LookupInfo {
+            int startIndex;
+            double t;
+            bool negate;
+        };
+
+        inline LookupInfo reduce(double x) {
+            x -= std::floor(x * invTwoPi) * twoPi;
+            bool negate = false;
+            if (x >= pi) {
+                x = twoPi - x;
+                negate = true;
+            }
+
+            double indexF = x * scale;
             int i1 = static_cast<int>(indexF);
             double t = indexF - i1;
 
-            // Wrap-safe indices
-            int i0 = (i1 - 1 + tableSize) % tableSize;
-            int i2 = (i1 + 1) % tableSize;
-            int i3 = (i1 + 2) % tableSize;
+            return { i1 + 3, t, negate };
+        }
 
-            return catmullRom(
-                table[i0], table[i1], table[i2], table[i3], t
-            );
+        inline double interpolate(const std::array<double, paddedSize>& table, const LookupInfo& info) {
+            const double* base = &table[info.startIndex - 1];
+            double value = catmullRom(base[0], base[1], base[2], base[3], info.t);
+            return info.negate ? -value : value;
         }
     }
 
     inline double sin(double x) {
-        return detail::interpolate(detail::sinTable, x);
+        return interpolate(sinTable, reduce(x));
     }
 
     inline double cos(double x) {
-        return detail::interpolate(detail::cosTable, x);
+        return interpolate(cosTable, reduce(x));
     }
 
-    inline void sincos(double x, double& outSin, double& outCos) {
-        x -= std::floor(x * invTwoPi) * twoPi;
-        double indexF = x * (tableSize * invTwoPi);
-        int i1 = static_cast<int>(indexF);
-        double t = indexF - i1;
-
-        // Wrap-safe indices
-        int i0 = (i1 - 1 + tableSize) % tableSize;
-        int i2 = (i1 + 1) % tableSize;
-        int i3 = (i1 + 2) % tableSize;
-
-        outSin = detail::catmullRom(
-            detail::sinTable[i0], detail::sinTable[i1],
-            detail::sinTable[i2], detail::sinTable[i3], t
-        );
-        outCos = detail::catmullRom(
-            detail::cosTable[i0], detail::cosTable[i1],
-            detail::cosTable[i2], detail::cosTable[i3], t
-        );
+    inline void sincos(double x, double& s, double& c) {
+        LookupInfo info = reduce(x);
+        s = interpolate(sinTable, info);
+        c = interpolate(cosTable, info);
     }
 }
