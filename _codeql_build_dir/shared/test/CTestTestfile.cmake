1a071da273271fb60634b13f7f9ad459563147b4
Nicolas <maerki@users.noreply.github.com>
2025-12-10 09:51:12 +0100

    delete codeql

--- _codeql_build_dir/shared/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/runner/work/maps-core/maps-core/shared/test
-# Build directory: /home/runner/work/maps-core/maps-core/_codeql_build_dir/shared/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/runner/work/maps-core/maps-core/_codeql_build_dir/shared/test/tests-b12d07c_include.cmake")
-subdirs("../../_deps/catch2-build")
