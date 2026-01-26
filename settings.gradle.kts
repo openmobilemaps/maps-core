f1fab0413e37357f71e1b9c7b98de49b7e5d3b01
Nicolas Märki <maerki@ubique.ch>
2026-01-26 09:29:07 +0100

    kmp: add settings with plugin repos

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+rootProject.name = "maps-core"
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
