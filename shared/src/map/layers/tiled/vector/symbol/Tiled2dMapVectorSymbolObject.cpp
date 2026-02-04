f2d0edb917091f3db658dc527d886da1ded512aa
Matthias Frei <frei@ubique.ch>
2026-02-04 08:31:46 +0100

    Revert merge error in Tiled2dMapVectorSymbolObject
    Went back and forth, but not changed intentionally in web map.

--- shared/src/map/layers/tiled/vector/symbol/Tiled2dMapVectorSymbolObject.cpp
+++ shared/src/map/layers/tiled/vector/symbol/Tiled2dMapVectorSymbolObject.cpp
@@ -539,9 +539,9 @@ void Tiled2dMapVectorSymbolObject::updateIconProperties(VectorModificationWrappe
         iconBoundingBoxViewportAligned.x = (-iconWidth * 0.5) - scaledIconPadding + offsets[2 * countOffset] * viewPortSize.x * 0.5;
         iconBoundingBoxViewportAligned.y = (-iconHeight * 0.5) - scaledIconPadding - offsets[2 * countOffset + 1] * viewPortSize.y * 0.5;
 
-        // TO_CHECK
-        offsets[2 * countOffset] += (iconOffset.value.x * iconSize.value) * 1.0 / viewPortSize.x;
-        offsets[2 * countOffset + 1] += (iconOffset.value.y * iconSize.value) * 1.0 / viewPortSize.y;
+        const double f = 2.0 / dpFactor;
+        offsets[2 * countOffset] += iconOffset.value.x * iconWidth * f / viewPortSize.x;
+        offsets[2 * countOffset + 1] += iconOffset.value.y * iconHeight * f / viewPortSize.y;
     } else {
         renderCoordinate = getRenderCoordinates(iconAnchor, -rotations[countOffset], iconWidth, iconHeight);
     }
@@ -554,8 +554,8 @@ void Tiled2dMapVectorSymbolObject::updateIconProperties(VectorModificationWrappe
         double sin, cos;
         lut::sincos(a, sin, cos);
 
-        const double iconOffsetX = iconOffset.value.x * iconSize.value * scaleFactor;
-        const double iconOffsetY = iconOffset.value.y * iconSize.value * scaleFactor;
+        const double iconOffsetX = iconOffset.value.x * iconWidth / dpFactor;
+        const double iconOffsetY = iconOffset.value.y * iconHeight / dpFactor;
 
         // add rotation offset
         x += iconOffsetX * cos - iconOffsetY * sin;
